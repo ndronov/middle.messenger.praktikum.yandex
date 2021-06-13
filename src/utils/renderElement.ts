d8c168c9dd38e699ea636dba222f45e9355e5fdc
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-06-13 13:17:38 +0700

    fix renderElement

--- src/utils/renderElement.ts
+++ src/utils/renderElement.ts
@@ -1,10 +1,10 @@
 const renderElement = (source: Element, target: Element): void => {
+  // eslint-disable-next-line no-param-reassign
+  target.innerHTML = '';
+
   Array.from(source.attributes).forEach(({ name, value }) => {
     target.setAttribute(name, value);
   });
-
-  // eslint-disable-next-line no-param-reassign
-  target.innerHTML = '';
 
   source.childNodes.forEach((childNode) => {
     setTimeout(() => target.append(childNode));
