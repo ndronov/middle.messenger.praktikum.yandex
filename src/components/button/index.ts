2a855515840243bee645943b0ff840fc4cc5dcaa
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-06-13 13:03:53 +0700

    button add TODO

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -2,6 +2,8 @@
 import pug from 'pug';
 import Component, { Props } from '../../modules/component';
 
+// TODO нужен ли этот компонент?
+
 const template = `
 button(class=className, id=id).some-class= child
 `;
@@ -9,11 +11,6 @@ button(class=className, id=id).some-class= child
 class Button extends Component {
   constructor(props: Props) {
     super('button', props);
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  componentDidMount(oldProps?: Props): void {
-    // TODO реализовать метод
   }
 
   render(): string {
