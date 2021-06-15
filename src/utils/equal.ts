06a8376df234dfa9196b59716ba5366a531d5825
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-06-15 18:29:31 +0700

    add equals util

---
+++ src/utils/equal.ts
@@ -0,0 +1,32 @@
+const equals = (a: unknown, b: unknown): boolean => {
+  if (a === b) {
+    return true;
+  }
+
+  if (a instanceof Date && b instanceof Date) {
+    return a.getTime() === b.getTime();
+  }
+
+  if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')) {
+    return a === b;
+  }
+
+  // @ts-ignore
+  if (a.prototype !== b.prototype) {
+    return false;
+  }
+
+  // @ts-ignore
+  const keysA = Object.keys(a);
+  // @ts-ignore
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  // @ts-ignore
+  return keysA.every((k: string) => equals(a[k], b[k]));
+};
+
+export default equals;
