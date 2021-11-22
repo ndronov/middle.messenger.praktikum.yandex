b2e6e779c7895095240192a9d0633be234de5a0a
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-11-22 17:18:29 +0700

    add component tests

---
+++ src/modules/component/component.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import Component from './index';
+
+// TODO доделать тесты
+
+describe('Component', () => {
+  it('Component is function', () => {
+    expect(typeof Component).to.eq('function');
+  });
+});
