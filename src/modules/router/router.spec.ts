43209abb3bc76026c17ac564e9c27a3472fae722
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-11-26 09:45:11 +0700

    add router tests

---
+++ src/modules/router/router.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import router from './index';
+
+// TODO доделать тесты
+
+describe('router', () => {
+  it('router is object', () => {
+    expect(typeof router).to.eq('object');
+  });
+});
