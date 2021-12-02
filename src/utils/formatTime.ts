d3ba633b4ae8e8fe42cfe93f349b971ac8a801de
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-12-02 07:02:22 +0700

    fix formatTime

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,7 +1,7 @@
 function formatTime(dateISO: string): string {
   const date = new Date(dateISO);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
 
   return `${hours}:${minutes}`;
 }
