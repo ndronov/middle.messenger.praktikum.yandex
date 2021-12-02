4706f4832afbf5c20afa3f45c33d5eed2e786efc
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-12-02 08:20:28 +0700

    fix server.js

--- server.js
+++ server.js
@@ -2,15 +2,22 @@
 const express = require('express');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-extraneous-dependencies
+const bodyParser = require('body-parser');
 
-const app = express();
 const DEFAULT_PORT = 3000;
 const port = process.env.PORT || DEFAULT_PORT;
 
-app.use(express.static(`${__dirname}/build`));
+const STATIC = path.resolve(__dirname, 'build');
+const INDEX = path.resolve(STATIC, 'index.html');
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const app = express();
+app.use(bodyParser.json());
+
+app.use(express.static(STATIC));
+
+app.get('*', (req, res) => {
+  res.sendFile(INDEX);
 });
 
 app.listen(port, () => {
