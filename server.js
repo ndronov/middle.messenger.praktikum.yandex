7686dc5b47b0c4789bcdc19a0aae4eeb37d5589c
Nikita Dronov <26296037+ndronov@users.noreply.github.com>
2021-12-02 08:05:37 +0700

    rework server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 const app = express();
 const DEFAULT_PORT = 3000;
 const port = process.env.PORT || DEFAULT_PORT;
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(`${__dirname}/build`));
 
-app.listen(port, function () {
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
